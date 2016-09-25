f74a41c369bdbbef592cb9f76b3864502892e07a
Richard Schneider <makaretu@gmail.com>
2016-09-26 12:03:20 +1300

    refactor(table): use a bridge.Game()

--- src/components/table-page/table.js
+++ src/components/table-page/table.js
@@ -3,17 +3,28 @@ define(["knockout", "bridge", "cards", "cards-ko", "jquery", "text!./table.html"
     function TableViewModel(route) {
         var self = this;
 
-        var hands = new bridge.Deck()
+        var board = new bridge.Board();
+        board.number = 666;
+        board.dealer = bridge.seat.south;
+        board.hands = new bridge.Deck()
             .shuffle()
             .deal(bridge.seat.south);
-        self.dealer = ko.observable(bridge.seat.south);
-        self.cards = ko.observableArray(hands.south.cards);
-        self.auction = ko.observableArray([bridge.bid['1C'], bridge.bid['-']])
+        var game = new bridge.Game();
+        game.auction.dealer = board.dealer;
+
+        self.board = ko.observable(board);
+        self.dealer = ko.observable(board.dealer);
+        self.cards = ko.observableArray(board.hands.south.cards);
+        self.auction = ko.observable(game.auction);
+        self.bids = ko.observableArray(game.auction.bids);
+        self.bidding = ko.pureComputed(function() {
+            return !self.auction().isClosed();
+        });
 
         self.play = function (data, event) {
             var card = bridge.card[cards.cid($(event.target))];
             self.cards.remove(card);
-        }
+        };
     }
 
     return { viewModel: TableViewModel, template: tableTemplate };
