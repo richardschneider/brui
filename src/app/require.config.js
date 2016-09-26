54da4450ce20a324cbade9162b4b28ab2598a5ae
Richard Schneider <makaretu@gmail.com>
2016-09-26 22:02:59 +1300

    refactor(modules): prefer a CDN instead of bower

--- src/app/require.config.js
+++ src/app/require.config.js
@@ -2,17 +2,17 @@
 var require = {
     baseUrl: ".",
     paths: {
-        "bootstrap":            "bower_modules/components-bootstrap/js/bootstrap.min",
+        "bootstrap":            "https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min",
+        "jquery":               "https://code.jquery.com/jquery-3.1.1.min",
+        "knockout":             "https://cdnjs.cloudflare.com/ajax/libs/knockout/3.4.0/knockout-min",
+        "bridge":				"https://unpkg.com/bridge.js/dist/bridge.min",
+        "cards":                "https://unpkg.com/cardsJS/dist/cards.min",
+        "cards-ko":             "https://unpkg.com/cardsJS/dist/cards-ko.min",
         "crossroads":           "bower_modules/crossroads/dist/crossroads.min",
         "hasher":               "bower_modules/hasher/dist/js/hasher.min",
-        "jquery":               "bower_modules/jquery/dist/jquery",
-        "knockout":             "bower_modules/knockout/dist/knockout",
         "knockout-projections": "bower_modules/knockout-projections/dist/knockout-projections",
         "signals":              "bower_modules/js-signals/dist/signals.min",
-        "bridge":				"https://unpkg.com/bridge.js/dist/bridge.min",
-        "text":                 "bower_modules/requirejs-text/text",
-        "cards":                "https://unpkg.com/cardsJS/dist/cards.min",
-        "cards-ko":             "https://unpkg.com/cardsJS/dist/cards-ko.min"
+        "text":                 "bower_modules/requirejs-text/text"
     },
     shim: {
         "bootstrap": { deps: ["jquery"] }
