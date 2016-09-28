8ff6a081c28e86d5af2b8fafe353d35b713d6f17
Richard Schneider <makaretu@gmail.com>
2016-09-28 18:32:18 +1300

    fix(dummy-hand): the trump suit starts the list of suits

--- src/components/dummy-hand/dummy-hand.js
+++ src/components/dummy-hand/dummy-hand.js
@@ -1,8 +1,19 @@
 define(['knockout', 'bridge', 'cards', 'cards-ko', 'text!./dummy-hand.html'], function(ko, bridge, cards, cardsKO, templateMarkup) {
 
     function DummyHand(params) {
+        var self = this;
         var dummy = params.contract().dummy();
-        this.hand = params.board().hands[dummy];
+        self.hand = params.board().hands[dummy];
+
+        self.suits = ko.pureComputed(function() {
+            var suits = ['S', 'H', 'D', 'C'];
+            var strain = params.contract().denomination;
+            if (strain != 'NT') {
+                suits.splice(suits.indexOf(strain), 1);
+                suits.unshift(strain);
+            }
+            return suits;
+        });
     }
 
   // This runs when the component is torn down. Put here any logic necessary to clean up,
